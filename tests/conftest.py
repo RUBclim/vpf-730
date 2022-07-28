e22caf191ae9913023a426d0016586c94e76cf93
Jonas Kittner <jonas.kittner@rub.de>
2022-07-28 19:50:16 +0200

    implement VPF730 class for reading measurements

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,31 @@ from uuid import UUID
 import pytest
 from freezegun import freeze_time
 
-from vpf_730.fifo_queue import Measurement
 from vpf_730.fifo_queue import Message
 from vpf_730.fifo_queue import Queue
+from vpf_730.vpf_730 import Measurement
+
+
+@pytest.fixture
+def measurement():
+    yield Measurement(
+        timestamp=1658758977000,
+        sensor_id=1,
+        last_measurement_period=60,
+        time_since_report=0,
+        optical_range=1.19,
+        precipitation_type_msg='NP',
+        obstruction_to_vision='HZ',
+        receiver_bg_illumination=0.06,
+        water_in_precip=0.0,
+        temp=20.5,
+        nr_precip_particles=0,
+        transmission_eq=2.51,
+        exco_less_precip_particle=2.51,
+        backscatter_exco=11.1,
+        self_test='OOO',
+        total_exco=2.51,
+    )
 
 
 @pytest.fixture
@@ -15,10 +37,10 @@ def queue(tmpdir):
 
 
 @pytest.fixture
-def queue_msg(queue):
+def queue_msg(queue, measurement):
     msg = Message(
         id=UUID('eb8ce9d920ff443b842eaf5f9d6b7486'),
-        blob=Measurement(timestamp=123456),
+        blob=measurement,
     )
     with freeze_time('2022-07-25 14:22:57'):
         queue.put(msg)
